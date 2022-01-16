04e2a15364f271c52799321f51be9dd4b331beff
Jeremiah James <jwjames83@gmail.com>
2022-01-15 20:45:50 -0500

    Fix up a few things and continue dev

--- components/sensors/src/sensor.h
+++ components/sensors/src/sensor.h
@@ -1,17 +1,32 @@
 #include "freertos/FreeRTOS.h"
+#include "driver/uart.h"
+
+typedef struct
+{
+    uint address_code;
+    uint function_code;
+    uint16_t starting_address;
+    uint16_t data_length;
+    uint crc_low;
+    uint crc_high;
+} request_message_t;
 
 class Sensor
 {
 public:
-    Sensor();
-    ~Sensor();
+    /**
+     * @brief Enum for baud rates
+     * 
+     */
+    typedef enum
+    {
+        BPS_2400 = 0x0000,
+        BPS_4800 = 0x0001,
+        BPS_9600 = 0x0002, // Default
+        BPS_DEFAULT = BPS_9600,
+    } Baudrate;
 
-    enum Baud {
-        BPS2400,
-        BPS4800,
-        BPS9600
-    };
-
+    /* ------------------------ funcs ------------------------ */
     /**
      * @brief Performs a query of all values on the sensor
      * 
@@ -19,68 +34,97 @@ public:
     void query_sensor();
 
     /**
-     * @brief Set the baud rate of the sensor
-     * 
-     * @param baud A Baud enum value
-     */
-    void set_baud_rate (Baud baud);
-
-    /**
      * @brief Get the current baud rate
      * 
      * @return Baud A baud enum value
      */
-    Baud get_baud_rate ();
+    Baudrate get_baud_rate();
 
-
-
-    /* -------------------- Register definitions --------------------*/
-    const uint REG_MOISTURE_CONTENT_RO = 0x0000;
-    const uint REG_TEMPERATURE_RO = 0x0001;
-    const uint REG_CONDUCTIVITY_RO = 0x0002;
-    const uint REG_PH_VALUE_RO = 0x0003;
-    const uint REG_NITROGEN_CONTENT_RO = 0x0004;
-    const uint REG_PHOSPHORUS_CONTENT_RO = 0x0005;
-    const uint REG_POTASSIUM_CONTENT_RO = 0x0006;
-    const uint REG_SALINITY_RO = 0x0007;
-    const uint REG_TDS_RO = 0x0008;
-
-    const uint REG_CONDUCTIVITY_TMP_COEFFICIENT_RW = 0x0023;
-    const uint REG_SALINITY_COEFFICIENT_RW = 0x0024;
-    const uint REG_TEMPERATURE_CALIBRATION_VALUE_RW = 0x0050;
-    const uint REG_WATER_CONTENT_CALIBRATION_VALUE_RW = 0x0051;
-    const uint REG_CONDUCTIVITY_CALIBRATION_VALUE_RW = 0x0052;
-    const uint REG_PH_CALIBRATION_VALUE_RW = 0x053;
-
-    const uint REG_NITROGEN_CONTENT_COEFFICIENT_HIGH_RW = 0x4E8;
-    const uint REG_NITROGEN_CONTENT_COEFFICIENT_LOW_RW = 0x4E9;
-    const uint REG_NITROGEN_CONTENT_CALIBRATION_VALUE_RW = 0x4E9;
-    
-    const uint REG_PHOSPHORUS_CONTENT_COEFFICIENT_HIGH_RW = 0x4F2;
-    const uint REG_PHOSPHORUS_CONTENT_COEFFICIENT_LOW_RW = 0x4F3;
-    const uint REG_PHOSPHORUS_CONTENT_CALIBRATION_VALUE_RW = 0x4F4;
-    
-    const uint REG_POTASSIUM_CONTENT_COEFFICIENT_HIGH_RW = 0x4FC;
-    const uint REG_POTASSIUM_CONTENT_COEFFICIENT_LOW_RW = 0x4FD;
-    const uint REG_POTASSIUM_CONTENT_CALIBRATION_VALUE_RW = 0x4FE;
-
-    const uint REG_DEVICE_ADDRESS_RW = 0x07D0;
-    const uint REG_DEVICE_BAUD_RATE_RW = 0x07D1;
-
-    const uint DEVICE_BAUD_2400 = 0x0000;
-    const uint DEVICE_BAUD_4800 = 0x0001;
-    const uint DEVICE_BAUD_9600 = 0x0002; // Default
+    Sensor();
+    Sensor(int uart_num, Baudrate baudrate);
+    ~Sensor();
 
 private:
-    struct request_message
-    {
-        uint address_code = 0x01;
-        uint function_code = 0x03;
-        uint16_t starting_address;
-        uint16_t data_length;
-        uint crc_low;
-        uint crc_high;
-    };
+    const char *TAG = "Soil Sensor";
 
-    void create_message (request_message* message);
+    /* ------------------------ types ------------------------ */
+    /**
+     * @brief Register addresses for reading moisture, temperature, conductivity, pH, NPK, salinity, and TDS (R)
+     * 
+     */
+    typedef enum
+    {
+        MOISTURE_CONTENT = 0x0000,
+        TEMPERATURE = 0x0001,
+        CONDUCTIVITY = 0x0002,
+        PH_VALUE = 0x0003,
+        NITROGEN_CONTENT = 0x0004,
+        PHOSPHORUS_CONTENT = 0x0005,
+        POTASSIUM_CONTENT = 0x0006,
+        SALINITY = 0x0007,
+        TDS = 0x0008,
+    } register_measurement;
+
+    /**
+     * @brief Register addresses for conductivity, salinity, temperature, moisture, conductivity, and pH coefficient and calibration values
+     * 
+     */
+    typedef enum
+    {
+        CONDUCTIVITY_TMP_COEFFICIENT = 0x0023,
+        SALINITY_COEFFICIENT = 0x0024,
+        TEMPERATURE_CALIBRATION_VALUE = 0x0050,
+        WATER_CONTENT_CALIBRATION_VALUE = 0x0051,
+        CONDUCTIVITY_CALIBRATION_VALUE = 0x0052,
+        PH_CALIBRATION_VALUE = 0x053,
+    } registers_calibration;
+
+    /**
+     * @brief Register addresses for nitrogen coefficient and calibration values
+     * 
+     */
+    typedef enum
+    {
+        NITROGEN_CONTENT_COEFFICIENT_HIGH = 0x4E8,
+        NITROGEN_CONTENT_COEFFICIENT_LOW = 0x4E9,
+        NITROGEN_CONTENT_CALIBRATION_VALUE = 0x4E9,
+    } registers_nitrogen;
+
+    /**
+     * @brief Register addresses for potassium coefficient and calibration values
+     * 
+     */
+    typedef enum
+    {
+        POTASSIUM_CONTENT_COEFFICIENT_HIGH = 0x4FC,
+        POTASSIUM_CONTENT_COEFFICIENT_LOW = 0x4FD,
+        POTASSIUM_CONTENT_CALIBRATION_VALUE = 0x4FE,
+    } registers_potassium;
+
+    /**
+     * @brief Register addresses for phosphorus coefficient and calibration values
+     * 
+     */
+    typedef enum
+    {
+        PHOSPHORUS_CONTENT_COEFFICIENT_HIGH = 0x4F2,
+        PHOSPHORUS_CONTENT_COEFFICIENT_LOW = 0x4F3,
+        PHOSPHORUS_CONTENT_CALIBRATION_VALUE = 0x4F4,
+    } register_phosphorus;
+
+    /**
+     * @brief Register addresses for device settings
+     * 
+     */
+    typedef enum
+    {
+        DEVICE_ADDRESS = 0x07D0,
+        DEVICE_BAUD_RATE = 0x07D1,
+    } registers_settings;
+
+    int m_uart_num = UART_NUM_1;
+
+    void write_message_bytes(request_message_t *message);
+    void send_bytes(uint8_t *bytes);
+    static void receive_task(void *params);
 };
